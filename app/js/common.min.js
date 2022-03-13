be8f6c3b17b2cbda9143ea364ecbdc3be5e783fd
Rokkido <rokkido2015@gmail.com>
2022-03-13 17:59:32 +0200

    first2

---
+++ app/js/common.min.js
@@ -0,0 +1,344 @@
+/*
+	hup 1.0.0
+	author: Rokkido
+	http://portfolio.rokkido.cx.ua/
+*/
+
+;(function(window){
+
+	window.hup = function(params){
+		let lang = params == undefined ? '' : params.lang;
+		let place = params == undefined ? '' : params.place;
+
+		let sitesArmy;
+		let sitesHumanitarian;
+
+		// sites army
+		if ( params.sitesArmy == undefined ) {
+			sitesArmy = [
+				{
+					title: {
+						en: 'Charitable fund "Come Back Alive"',
+						ua: 'Благодійний фонд "Повернись живим"',
+						ru: 'Благотворительный фонд "Вернись живым"'
+					},
+					description: {
+						en: 'One of the biggest charity funds helping Ukrainian soldiers.',
+						ua: 'Один із найбільших благодійних фондів, який допомагає українським військовим.',
+						ru: 'Один из крупнейших благотворительных фондов, помогающий украинским военным.',
+					},
+					currency: 'Fondy.eu, Swift, Cryptocurrency (BTC, ETH, USDC, USDT), UAH',
+					link: 'https://www.comebackalive.in.ua/donate'
+				},
+				{
+					title: {
+						en: 'National Bank of Ukraine',
+						ua: 'Національний банк України',
+						ru: 'Национальный банк Украины'
+					},
+					description: {
+						en: 'The National Bank of Ukraine opened a bank account to raise money for the army needs.',
+						ua: 'Національний банк України відкрив рахунок для збирання коштів на потреби армії.',
+						ru: 'Национальный банк Украины открыл счет для сбора средств на нужды армии.'
+					},
+					currency: 'USD, EUR, GBP, CHF, AUD, CAD, PLN, UAH',
+					link: {
+						en: 'https://bank.gov.ua/en/about/support-the-armed-forces',
+						ua: 'https://bank.gov.ua/ua/about/support-the-armed-forces',
+						ru: 'https://bank.gov.ua/en/about/support-the-armed-forces'
+					}
+				},
+				{
+					title: {
+						en: 'monobank | UniversalBank',
+						ua: 'monobank | UniversalBank',
+						ru: 'monobank | UniversalBank'
+					},
+					description: {
+						en: 'Funds are raised onto a single account of the National bank of Ukraine created to support the Ukrainian army.',
+						ua: 'Кошти збираються на єдиний рахунок у Національному банку України, створений для підтримки української армії.',
+						ru: 'Средства собираются на единый счет в Национальном банке Украины, созданный для поддержки украинской армии.'
+					},
+					currency: 'Any bank, Apple Pay, Google Pay',
+					link: 'https://uahelp.monobank.ua'
+				},
+				{
+					title: {
+						en: 'Cryptocurrency',
+						ua: 'Криптовалюта',
+						ru: 'Криптовалюта'
+					},
+					description: {
+						en: 'Official crypto wallets for transferring money for the Ukrainian army.',
+						ua: 'Офіційні криптогаманці, куди можна переказати гроші для армії України.',
+						ru: 'Официальные криптокошельки, куда можно перевести деньги для армии Украины.'
+					},
+					currency: 'BTC, ETH, USDT, DOT, SOL, DOGE',
+					link: 'https://thedigital.gov.ua/news/ofitsiyni-kriptogamantsi-kudi-mozhna-perekazati-groshi-dlya-armii-ukraini'
+				}
+			];
+		} else {
+			sitesArmy = params.sitesArmy;
+		}
+
+		// sites humanitarian
+		if ( params.sitesHumanitarian == undefined ) {
+			sitesHumanitarian = [
+				{
+					title: {
+						en: 'Red Cross',
+						ua: 'Червоний Хрест',
+						ru: 'Красный Крест'
+					},
+					description: {
+						en: 'All raised funds will be used to support victims, blood donation, mobilization of volunteers and resources.',
+						ua: 'Всі залучені кошти будуть використані на підтримку постраждалих, донорство крові, мобілізацію волонтерів та ресурсів.',
+						ru: 'Все привлеченные средства будут использованы в поддержку пострадавших, донорство крови, мобилизацию волонтеров и ресурсов.'
+					},
+					currency: 'USD, EUR, UAH',
+					link: {
+						en: 'https://donate.redcrossredcrescent.org/ua/donate/~my-donation',
+						ua: 'https://donate.redcrossredcrescent.org/ua/donate/~%D0%BC%D0%BE%D1%8F-%D0%BF%D0%BE%D0%B6%D0%B5%D1%80%D1%82%D0%B2%D0%B0',
+						ru: 'https://donate.redcrossredcrescent.org/ua/donate/~my-donation'
+					}
+				},
+				{
+					title: {
+						en: 'National Bank of Ukraine',
+						ua: 'Національний банк України',
+						ru: 'Национальный банк Украины'
+					},
+					description: {
+						en: 'The National Bank of Ukraine opened a bank account for raising money for humanitarian help.',
+						ua: 'Національний банк України відкрив рахунок для збирання коштів на гуманітарну допомогу.',
+						ru: 'Национальный банк Украины открыл счет для сбора средств на гуманитарную помощь.'
+					},
+					currency: 'USD, EUR, GBP, CHF, AUD, CAD, PLN, UAH',
+					link: {
+						en: 'https://bank.gov.ua/en/about/humanitarian-aid-to-ukraine',
+						ua: 'https://bank.gov.ua/ua/about/humanitarian-aid-to-ukraine',
+						ru: 'https://bank.gov.ua/en/about/humanitarian-aid-to-ukraine'
+					}
+				},
+				{
+					title: {
+						en: 'Unchain.Fund',
+						ua: 'Unchain.Fund',
+						ru: 'Unchain.Fund'
+					},
+					description: {
+						en: 'Unchain is a charity project created by blockchain activists.',
+						ua: 'Unchain — це благодійний проект, створений активістами блокчейн.',
+						ru: 'Unchain — благотворительный проект, созданный активистами блокчейн.'
+					},
+					currency: 'BTC, ETH, BSC, Polygon, Harmony, NEAR, Avalanche, Celo, Terra, IoTeX',
+					link: 'https://unchain.fund/donate'
+				}
+			]
+		} else {
+			sitesHumanitarian = params.sitesHumanitarian;
+		}
+
+		// lang
+		if (lang != 'en' && lang !== 'ua' && lang !== 'ru') {
+			lang = 'en';
+		}
+
+		// btn place
+		if (place != 'right' && place != 'center' && place !== 'left') {
+			place = 'right';
+		}
+
+		const utm = '?utm_source=HelpUkrainePopup';
+		const $body = $('body');
+		
+		const dataText = {
+			header: {
+				en: 'Help for Ukraine in the war with Russia',
+				ua: 'Допомога Україні у війні з Росією',
+				ru: 'Помощь Украине в войне с Россией'
+			},
+			help: {
+				en: 'Whom do you want to help?',
+				ua: 'Кому ви хочете допомогти?',
+				ru: 'Кому вы хотите помочь?'
+			}
+		}
+
+		const dataContent = [
+			{
+				category: 'army',
+				title: {
+					en: 'Help to the Ukrainian army',
+					ua: 'Допомога армії України',
+					ru: 'Помощь армии Украины'
+				},
+				name: {
+					en: 'The armed forces',
+					ua: 'Збройні сили',
+					ru: 'Вооруженные силы'
+				},
+				sites: sitesArmy
+			},
+			{
+				category: 'humanitarian',
+				title: {
+					en: 'Humanitarian aid',
+					ua: 'Гуманітарна допомога',
+					ru: 'Гуманитарная помощь'
+				},
+				name: {
+					en: 'Humanitarian aid',
+					ua: 'Гуманітарна допомога',
+					ru: 'Гуманитарная помощь'
+				},
+				sites: sitesHumanitarian
+			}
+		];
+
+		const markup = `
+			<div class="hup">
+				<div class="hup-wrapper">
+					<div class="hup-overlay"></div>
+					<div class="hup-block">
+						<div class="hup-header">
+							<div class="hup-back"></div>
+							<span>${ getLangString(dataText.header) }</span>
+						</div>
+						<div class="hup-content">
+							<div class="hup-section hup-section--start active">
+								<div class="hup-top-img hup-bg-img hup-bg-img--emblem"></div>
+								<div class="hup-title">${ getLangString(dataText.help) }</div>
+								<div class="hup-items">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+
+		$body.insertAdjacentHTML('beforeend', markup); // markup to body
+		
+		const $popup = $('.hup');
+		const $wrapper = $('.hup-wrapper');
+		const $back = $('.hup-back');
+		const $content = $('.hup-content');
+		const $itemsStart = $('.hup-section--start .hup-items');
+
+		dataContent.forEach(val => {
+			const $buttonInner = `
+				<div class="hup-img hup-bg-img hup-bg-img--${val.category}"></div>
+				<div class="hup-tit">${ getLangString(val.name) }</div>
+			`;
+
+			const $button = document.createElement('button');
+			$button.classList.add('hup-item');
+			$button.setAttribute('data-category', val.category);
+			$button.addEventListener('click', () => setSection(val.category));
+			
+			$button.insertAdjacentHTML('beforeend', $buttonInner);
+
+			$itemsStart.append($button);
+
+			// create inner sections
+			const $sectionMarkup = `
+				<div class="hup-section hup-section--inner hup-section--${val.category}">
+					<div class="hup-top-img hup-bg-img hup-bg-img--${val.category}"></div>
+					<div class="hup-title">${ getLangString(val.title) }</div>
+					<div class="hup-items">
+						
+					</div>
+				</div>
+			`;
+			
+			const $section = new DOMParser().parseFromString($sectionMarkup, "text/html").firstChild.childNodes[1].childNodes[0];
+			const $itemsInner = $section.querySelector('.hup-items');
+			let $sites = '';
+
+			val.sites.forEach(site => {
+				$sites += `
+					<a class="hup-item" href="${ getLangString(site.link) + utm }" target="_blank">
+						<div class="hup-tit">${ getLangString(site.title) }</div>
+						<div class="hup-p">${ getLangString(site.description) }</div>
+						<div class="hup-currency">${ getLangString(site.currency) }</div>
+					</a>
+				`;
+			});
+
+			$itemsInner.insertAdjacentHTML('beforeend', $sites);
+
+			$content.append($section);
+		});
+
+
+		// create button
+		const $button = createEl('hup-button', 'button');
+		$button.classList.add(`hup-${place}`);
+		$button.addEventListener('click', togglePopup); // add event
+		$body.append($button); // button to body
+
+		// add events
+		$wrapper.addEventListener('click', function(e){
+			if (e.target.className != 'hup-wrapper') return;
+
+			togglePopup();
+			setTimeout(() => setSection('start'), 500);
+		});
+
+		$back.addEventListener('click', () => setSection('start'));
+
+		// toggle popup
+		function togglePopup() {
+			$popup.classList.toggle('active');
+		}
+
+		// create element
+		function createEl(clas, tag = 'div'){
+			const $el = document.createElement(tag);
+			$el.classList.add(clas);
+			return $el;
+		}
+
+		// query selector class
+		function $(el) {
+			return document.querySelector(el);
+		}
+
+		// go inner category
+		function setSection(category) {
+			const $sections = document.querySelectorAll('.hup-section');
+			const $currentSection = document.querySelector(`.hup-section--${category}`);
+
+			$sections.forEach($section => $section.classList.remove('active'));
+			$currentSection.classList.add('active');
+
+			if (category == 'start') {
+				$back.classList.remove('active');
+			} else {
+				$back.classList.add('active');
+			}
+		}
+
+
+		vh100();
+		window.addEventListener('resize', vh100);
+
+		function vh100(){
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--hup-vh', `${vh}px`);
+		}
+
+		function getScrollbarWidth() {
+			return window.innerWidth - document.documentElement.clientWidth;
+		}
+
+
+		// string or obj
+		function getLangString(data) {
+			return typeof(data) == 'string' ? data : data[lang]
+		}
+	}
+
+})(window);
---
+++ gulpfile.js
@@ -0,0 +1,173 @@
+const fs            = require('fs')
+const gulp          = require('gulp')
+const sass          = require('gulp-sass')
+const browserSync   = require('browser-sync')
+const autoprefixer  = require('gulp-autoprefixer')
+const notify        = require('gulp-notify')
+const concat        = require('gulp-concat')
+const pug           = require('gulp-pug')
+const uglify        = require('gulp-uglify-es').default
+const cleanCSS      = require('gulp-clean-css')
+const realFavicon   = require('gulp-real-favicon')
+const gulpif        = require('gulp-if')
+const qrcode        = require('qrcode-terminal')
+const rename        = require('gulp-rename')
+var injectCSS       = require('gulp-css-to-js-style');
+
+
+const allMinify = false // минифицировать все файлы
+
+
+// Sass
+gulp.task('styles', function() {
+	return gulp.src(`app/_dev/sass/**/*.sass`)
+		.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+		.pipe(rename({ suffix: '.min', prefix : '' }))
+		.pipe(autoprefixer(['last 2 versions']))
+		.pipe( gulpif(allMinify, cleanCSS({level: { 1: { specialComments: 0 } } })) )
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream())
+})
+
+// Js
+gulp.task('scripts', function() {
+	return gulp.src([
+		`app/_dev/js/common.js` // Всегда в конце
+		])
+		.pipe(concat('common.min.js'))
+		.pipe( gulpif(allMinify, uglify()) ) // Минификатор
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.stream())
+})
+
+// Pug
+gulp.task('pug', function() {
+	return gulp.src(`app/_dev/pug/*.pug`)
+		.pipe(pug({
+			pretty: '\t'
+		}))
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.stream())
+})
+
+// Browser-Sync
+gulp.task('browser-sync', function() {
+	browserSync({
+		callbacks: {
+			ready: function(err, bs){
+				// QR code terminal: external link
+				// const qrExternalLink = bs.options.get('urls').get('external')
+
+				// qrcode.generate(qrExternalLink, {small: true})
+			}
+		},
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		port: '5566',
+		host: '192.168.1.121', // Сменить адрес, если сервер не работает на мобильном 
+		// online: true, // Если нет интернета
+		// tunnel: true, tunnel: "developerrokkido" // Демонстрация заказчику: http://developerrokkido.localtunnel.me
+	})
+})
+
+
+
+// Favicon generator
+gulp.task('favicon', function(done) {
+	const faviconDataPath = `app/img/favicon/faviconData.json`
+
+	if ( !fs.existsSync(faviconDataPath) ) { // если фавиконки не созданы - создать
+
+		let faviconImgPath = `app/img/favicon/favicon.svg`
+
+		if (!fs.existsSync(faviconImgPath)) {
+			faviconImgPath = `app/img/favicon/favicon.png`
+		}
+
+		realFavicon.generateFavicon({
+			masterPicture: faviconImgPath,
+			dest: `app/img/favicon`,
+			iconsPath: '/',
+			design: {
+				ios: {
+					pictureAspect: 'noChange',
+					assets: {
+						ios6AndPriorIcons: false,
+						ios7AndLaterIcons: false,
+						precomposedIcons: false,
+						declareOnlyDefaultIcon: true
+					}
+				},
+				desktopBrowser: {
+					design: 'raw'
+				},
+				windows: {
+					pictureAspect: 'noChange',
+					backgroundColor: '#666f86',
+					onConflict: 'override',
+					assets: {
+						windows80Ie10Tile: false,
+						windows10Ie11EdgeTiles: {
+							small: false,
+							medium: true,
+							big: false,
+							rectangle: false
+						}
+					}
+				},
+				androidChrome: {
+					pictureAspect: 'noChange',
+					themeColor: '#ffffff',
+					manifest: {
+						name: 'Sitename',
+						display: 'standalone',
+						orientation: 'notSet',
+						onConflict: 'override',
+						declared: true
+					},
+					assets: {
+						legacyIcon: false,
+						lowResolutionIcons: false
+					}
+				},
+				safariPinnedTab: {
+					pictureAspect: 'blackAndWhite',
+					threshold: 75,
+					themeColor: '#666f86'
+				}
+			},
+			settings: {
+				scalingAlgorithm: 'Mitchell',
+				errorOnImageTooSmall: false,
+				readmeFile: false,
+				htmlCodeFile: false,
+				usePathAsIs: false
+			},
+			markupFile: faviconDataPath
+		}, function() {
+			done()
+		})
+	}
+})
+
+// build
+gulp.task('css-to-js', function(){
+	gulp.src('./src/**.css')
+		.pipe(injectCSS())
+		.pipe(concat('injected-styles.js'))
+		.pipe(gulp.dest('./dist'));
+});
+
+
+// Watch
+gulp.task('watch', function(){
+	gulp.watch(`app/_dev/sass/**/*.sass`, gulp.parallel('styles'))
+	gulp.watch(`app/_dev/js/**/*.js`, gulp.parallel('scripts'))
+	gulp.watch(`app/_dev/pug/**/*.pug`, gulp.parallel('pug'))
+})
+
+
+// gulp default
+gulp.task('default', gulp.parallel('styles', 'scripts', 'pug', 'browser-sync', 'watch', 'favicon'))
